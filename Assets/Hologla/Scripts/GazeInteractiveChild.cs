b430e2f6a5a75b783088c0aff229315044ae98e2
Naoaki-Y <diberidarshi@yahoo.co.jp>
2021-06-11 08:49:16 +0900

    Gazeによるインタラクション使用時に、ゲームオブジェクトの階層上、子要素がコリジョンを持っていて反応させたい場合用に、親のインタラクションイベントを呼ぶコンポーネントを追加

---
+++ Assets/Hologla/Scripts/GazeInteractiveChild.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.Events;
+
+
+namespace Hologla{
+
+	// 親階層にGazeInteractiveコンポーネントがある場合、その各イベントを呼ぶ.
+	public class GazeInteractiveChild : MonoBehaviour, IGazeInteract
+	{
+		[SerializeField]private IGazeInteract parentGazeInteractive = null;
+
+		private void Awake( )
+		{
+			if( null == parentGazeInteractive ){
+				parentGazeInteractive = transform.parent.GetComponent<IGazeInteract>( );
+			}
+
+			return;
+		}
+
+		public void OnClick(ClickType clickType)
+		{
+			if( null != parentGazeInteractive ){
+				parentGazeInteractive.OnClick(clickType);
+			}
+			return;
+		}
+
+		public void OnDeselect( )
+		{
+			if( null != parentGazeInteractive ){
+				parentGazeInteractive.OnDeselect( );
+			}
+			return;
+		}
+
+		public void OnSelect( )
+		{
+			if( null != parentGazeInteractive ){
+				parentGazeInteractive.OnSelect( );
+			}
+			return;
+		}
+	}
+
+}
